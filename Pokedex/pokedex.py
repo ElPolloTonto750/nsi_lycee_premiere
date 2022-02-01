6e540b76b6fcd608c17226aeec4c7b74cb3ec017
Swann Merienne <59657132+SwannMrn@users.noreply.github.com>
2022-02-01 10:39:18 +0000

    added functions and formulas

--- Pokedex/pokedex.py
+++ Pokedex/pokedex.py
@@ -1,3 +1,23 @@
 from math import *
 
-def
+import csv
+chart1 = []
+with open('pokemon.csv') as f:
+    c = csv.DictReader(f, delimiter=',')
+    for line in c:
+        chart.append(line)
+
+atk = chart[pokemon]['nb_hab_2012']
+
+def pokemon_nbr(pokemon: str)-> int:
+	"""takes a pokemon name and returns his index number"""
+    l = []
+    f = open("pokemon.csv", "r")
+    c = csv.DictReader(f, delimiter=',')
+    for line in c:
+        if line['name'] == pokemon
+            pokemon_index = line['#']
+	return pokemon_index
+
+def damage(pokemon):
+	dmg = ((12/5)*/def)*type
