b26badc3f944975253a7a1f97109a03c8916d245
Swann Merienne <59657132+SwannMrn@users.noreply.github.com>
2022-02-01 11:15:13 +0000

    functions go brrrrr

--- Pokedex/pokedex.py
+++ Pokedex/pokedex.py
@@ -7,18 +7,21 @@ with open('pokemon.csv') as f:
     for line in c:
         chart.append(line)
 
-def pokemon_nbr(pokemon: str)-> int:
+def pokemon_index(pokemon: str)-> int:
     """takes a pokemon name and returns his index number"""
     l = []
     f = open("pokemon.csv", "r")
     c = csv.DictReader(f, delimiter=',')
     for line in c:
         if line['Name'] == pokemon:
-            pokemon_index = line['#']
-    return pokemon_index
+            pokemon_nbr = int(line['#'])
+    return pokemon_nbr
 
 def damage(pokemon):
-    atk = chart[pokemon]['Attack']
-    defense = chart[pokemon]['Defense']
-    dmg = ((12/5)*atk/defense)
+    pokemon_nbr = pokemon_index(pokemon)
+    atk = int(chart[pokemon_nbr]['Attack'])
+    defense = int(chart[pokemon_nbr]['Defense'])
+    print(atk)
+    print(defense)
+    dmg = ((2.4*atk)/(defense/5))
     return dmg
