84438384e64b30d0790ec8f3620a1ceec105f9d7
Swann Merienne <59657132+SwannMrn@users.noreply.github.com>
2022-02-06 16:55:47 +0000

    added and modified functions, deleted a useless pdf and added info in README

--- Pokedex/README.txt
+++ Pokedex/README.txt
@@ -1,4 +1,7 @@
 Project Pokedex_and_BattleSimulator
-how to run python on replit :
-https://replit.com/talk/ask/How-to-run-another-python-file-not-the-mainpy/29188
 
+GitHub:
+https://github.com/ElPolloTonto750/nsi_lycee_premiere
+
+How to run python on replit :
+https://replit.com/talk/ask/How-to-run-another-python-file-not-the-mainpy/29188
--- Pokedex/pokedex.py
+++ Pokedex/pokedex.py
@@ -1,10 +1,14 @@
 from math import *
+from random import *
 import csv
 chart = []
 with open("pokemon.csv") as f:
     c = csv.DictReader(f, delimiter=',')
     for line in c:
         chart.append(line)
+
+#import modules, files and lists
+
 
 
 def pokemon_index(pokemon: str)-> int:
@@ -18,29 +22,46 @@ def pokemon_index(pokemon: str)-> int:
     return pokemon_nbr
 
 
-def atk(pokemon):
+
+def atk(pokemon: str)-> int:
+	"""takes a pokemon string and returns his attack"""
     pokemon_nbr = pokemon_index(pokemon)
     atk = int(chart[pokemon_nbr]['Attack'])
     return atk
 
 
-def defense(pokemon):
+
+def defense(pokemon: str)-> int:
+	"""takes a pokemon string and returns his defense"""
     pokemon_nbr = pokemon_index(pokemon)
     defense = int(chart[pokemon_nbr]['Defense'])
     return defense
 
+
     
-def damage(pokemon):
+def damage(pokemon: str)-> float:
+	"""takes a pokemon string and returns his damage"""
     pokemon_nbr = pokemon_index(pokemon)
     atk = int(chart[pokemon_nbr]['Attack'])
     defense = int(chart[pokemon_nbr]['Defense'])
+		bonus_dmg = random.randint(0,20)
     print(atk)
     print(defense)
+		print(bonus_dmg)
     dmg = ((2.4*atk)/(defense/5))
     return dmg
 
-"""
-def whos_faster(chart[pokemon_nbr], chart[pokemon_nbr2]):
-"""
 
 
+def fastest_pokemon(pokemon1: str, pokemon2: str)-> str:
+	"""takes 2 pokemon strings and returns the fastest pokemon"""
+	randlist = [pokemon1; pokemon2]
+	pokemon1_nbr = pokemon_index(pokemon)
+	pokemon2_nbr = pokemon_index(pokemon)
+	atksp1 = int(chart[pokemon1_nbr]['Sp. Atk']
+	atksp2 = int(chart[pokemon2_nbr]['Sp. Atk']
+	if atksp1 > atksp2:
+		fastestpok = pokemon1
+	elif atksp1 = atksp2:
+		fastestpok = random.choice(randlist)
+	return fastestpok
