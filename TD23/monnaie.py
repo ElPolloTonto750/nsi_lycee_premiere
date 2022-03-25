80377c9e5b6ca773628c2dc1bbc73ef150cc62ea
Increible_pollito <69044505+ElPolloTonto750@users.noreply.github.com>
2022-03-25 10:07:06 +0100

    Update monnaie.py
    changes

--- TD23/monnaie.py
+++ TD23/monnaie.py
@@ -1,10 +1,11 @@
 def monnaie(m):
-    S = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+    S1 = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+    S2 = [1000, 100, 10, 1]
     T = []
     while m != 0:
-        for i in S:
+        for i in S1: #S1 can be changed into S2 or S3
             while m >= i:
                 T.append(i)
                 m -= i
     return T
-# print(monnaie(654))
+# print(monnaie(1647))
